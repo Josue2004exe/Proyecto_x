1bd08f005b6328c0501639219dff813e2aa7f833
Josue2004exe <alexpc778@gmail.com>
2026-05-28 18:00:13 -0500

    corregido

--- utils/validaciones.py
+++ utils/validaciones.py
@@ -10,30 +10,39 @@ def validar_campos_obligatorios(datos):
     return True, "Datos válidos."
 
 
-def pedir_texto(mensaje):
+def pedir_texto(mensaje, validacion_extra= None):
     while True:
-        valor = input(mensaje)
+        valor = input(mensaje).strip()
+        if campo_vacio(valor):
+            print("Este campo no puede estar vacio")
+            continue
+        if validacion_extra:
+            es_valido, resultado = validacion_extra(valor)
+            if not es_valido:
+                print("Error: El formato o número ingresado es inválido. Intente de nuevo.")
+                continue
+            return resultado
+        return valor
 
-        if not campo_vacio(valor):
-            return valor
 
-        print("Este campo no puede estar vacío.")
-
-
-def pedir_entero(mensaje):
+def pedir_entero(mensaje, validacion_extra=None):
     while True:
-        valor = input(mensaje)
-
+        valor = input(mensaje).strip()
         try:
-            return int(valor)
+            numero =  int(valor)
+
+            if validacion_extra is not None:
+                if not validacion_extra(numero):
+                    print("Error: El valor no cumple con los requisitos del sistema Intente de nuevo.")
+                    continue
+            return numero
         except ValueError:
             print("ERROR: Debe ingresar un número entero válido.")
 
 
 def pedir_decimal(mensaje):
     while True:
-        valor = input(mensaje)
-
+        valor = input(mensaje).strip()
         try:
             return float(valor)
         except ValueError:
@@ -81,3 +90,19 @@ def validar_identificacion_ecuatoriana(documento):
     if digito_esperado == verificador:
         return True, doc 
     return False, None
+
+def pedir_cantidad_stock(mensaje, tipo_movimiento, stock_disponible):
+    while True:
+        cantidad = pedir_entero(mensaje)
+        
+        if cantidad == 0:
+            return 0
+        if cantidad < 0:
+            print("Error: La cantidad debe ser mayor a cero. Intente de nuevo.")
+            continue
+
+        if tipo_movimiento == "VENTA" and stock_disponible < cantidad:
+            print(f"Error: Stock insuficiente. Stock actual disponible: {stock_disponible}. Intente otra cantidad.")
+            continue
+
+        return cantidad
